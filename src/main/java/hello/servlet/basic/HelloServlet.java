6c02bedc1b205a1b9f3b9703f7625d3a5615a461
Nexusism <wanggoog91@gmail.com>
2022-02-14 17:54:44 +0900

    HTTP 요청 데이터 - 개요 2

--- src/main/java/hello/servlet/basic/HelloServlet.java
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -1,2 +1,28 @@
-package hello.servlet.basic;public class HelloServlet {
+package hello.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username"); // 쿼리파라미터 조회
+        System.out.println("username = " + username);
+
+        // 응답부분
+        response.setContentType("text/plain");   // 헤더정보
+        response.setCharacterEncoding("UTF-8");  // 헤더정보
+        response.getWriter().write("hello" + username); //HTTP 메시지 바디에 들어감
+    }
 }
